82f4fc4f30dd6b1a979745ac93253359fa71f6cd
Nana K <73991180+topboyasante@users.noreply.github.com>
2022-12-12 22:00:21 +0000

    Update

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,26 +9,30 @@ function Cart() {
         <section className='max-w-[1240px] mx-auto h-[93vh]'>
             <h1 className='text-center text-xl my-2'>Cart</h1>
             <hr />
+            {cart.length > 0 ?
             <section className='my-5 flex flex-col gap-5 p-5'>
-                {cart.map((item)=>{
-                    return(
-                        <section className='flex justify-around items-center border p-3'>
-                            <div className='w-[45%] flex justify-around items-center'>
-                                <p>{item.name}</p>
-                                <div className='flex gap-3'>
-                                    <button>-</button>
-                                    <p className='text-center'>{item.qtyInCart}</p>
-                                    <button>+</button>
-                                </div>
+            {cart.map((item)=>{
+                return(
+                    <section className='flex justify-around items-center border p-3'>
+                        <div className='w-[45%] flex justify-around items-center'>
+                            <p>{item.name}</p>
+                            <div className='flex gap-3'>
+                                <button>-</button>
+                                <p className='text-center'>{item.qtyInCart}</p>
+                                <button>+</button>
                             </div>
-                            <div className='w-[45%] flex items-center justify-around'>
-                            <p>Price: ${item.price}</p>
-                            <RiDeleteBin6Line/>
-                            </div>
-                        </section>
-                    )
-                })}
-            </section>
+                        </div>
+                        <div className='w-[45%] flex items-center justify-around'>
+                        <p>Price: ${item.price}</p>
+                        <RiDeleteBin6Line/>
+                        </div>
+                    </section>
+                )
+            })}
+        </section>:
+        <section>
+            <p className='text-center text-2xl py-5 text-gray-400'>Cart is Empty.</p>
+        </section>}
         </section>
    </main>
   )
